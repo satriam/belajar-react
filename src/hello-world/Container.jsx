8ae7bc60e9c8e64efba211810370bdfed8821ad4
Satt <satriabae43@gmail.com>
2025-02-26 18:48:00 +0700

    nested container

---
+++ src/hello-world/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({children}){
+return(
+    <div>
+        <h1>Satria Mulya </h1>
+        {children}
+        <footer>
+            <p><p>Satt</p></p>
+        </footer>
+    </div>
+)
+}
--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld";
+import Container from "./Container";
 
 createRoot(document.getElementById("root"))
     .render(
         <StrictMode>
+           <Container>
             <HelloWorld/>
+           </Container>
         </StrictMode>
     )
