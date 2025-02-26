60185320ff0f313dee7b4b9e3b3cb5b2358fd1c1
Satria <satriabae43@gmail.com>
2025-02-26 09:24:22 +0700

    slice componet with function

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,8 +1,20 @@
 export default function HelloWorld(){
     return (
         <div>
-            <h1>Belajar React JS Dasar</h1>
-            <p>hello</p>
+            <Header/>
+            <Paragraf/>
         </div>
+    )
+}
+
+function Header(){
+    return(
+        <h1>Belajar React JS Dasar</h1>
+    )
+}
+
+function Paragraf(){
+    return (
+        <p>hello</p>
     )
 }
