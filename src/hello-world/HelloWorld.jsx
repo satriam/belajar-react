546f03a0a671b7995beeeea8068e46121c72e082
Satria <satriabae43@gmail.com>
2025-02-26 09:13:59 +0700

    tahap Hello world component

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,8 @@
+export default function HelloWorld(){
+    return (
+        <div>
+            <h1>Belajar React JS Dasar</h1>
+            <p>hello</p>
+        </div>
+    )
+}
---
+++ src/hello-world/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./HelloWorld";
+
+createRoot(document.getElementById("root"))
+    .render(
+        <StrictMode>
+            <HelloWorld/>
+        </StrictMode>
+    )
